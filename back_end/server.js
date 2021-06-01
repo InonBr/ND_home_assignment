16e14d53901f40a17db3fba95529c2a5139b77fb
Inon Bercu <inonbr@gmail.com>
2021-06-01 16:49:57 +0300

    got some random data to feed the chrts on the front end

--- back_end/server.js
+++ back_end/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectDB = require('./db/db');
 
 const userRouters = require('./routers/users');
+const randomData = require('./routers/randomData');
 
 const port = 5000;
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRouters);
+app.use('/api', randomData);
 
 /**
  * connecting to mongoDB
